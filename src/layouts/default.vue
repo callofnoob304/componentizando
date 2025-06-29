d0383de06017e39f211d538eeed2b97773e23bed
Felipe <callofnoob712@gmail.com>
2025-06-28 21:10:44 -0300

    Iniciando o projeto de componentes

---
+++ src/layouts/default.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <v-app-bar>
+      <h2>Meus exemplos de componentes</h2>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts" setup>
+  //
+</script>
